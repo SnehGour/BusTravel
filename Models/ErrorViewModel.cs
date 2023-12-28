cd7d300746cc365d0503c9d9427f99f635374116
Sneh Gour <sneh.gour@rws.com>
2023-12-28 12:49:36 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusTravel.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
